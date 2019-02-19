64025554a2224eb05f36a871bd6aaf6959e6e1df
Ivan <ivanlins73@gmail.com>
2019-02-19 15:32:56 -0300

    Manipulando Linhas (UPDATE, DELETE e TRUNCATE)

---
+++ Mysql/Tabela - Alterando e modificando linhas.sql
@@ -0,0 +1,14 @@
+use cadastro;
+insert into cursos values
+('1', 'HTML5', 'CURSO DE HTML5', '40', '36', '2014'),
+('2', 'ALGORITMOS', 'LOGICA DE PROGRAMAÇÃO', '20', '15', '2014'),
+('3', 'PHOTOSHOP', 'DICAS DE PHOTOSHOP CC', '10', '8', '2014'),
+('4', 'PGP', 'CURSO DE PHP PARA INICIANTES', '40', '20', '2010'),
+('5', 'JARVA', 'INTRODUÇÃO A LINGUAGEM JAVA', '10', '8', '2000'),
+('6', 'MySQL', 'BANCO DE DADOS MySQL', '30', '15', '2016'),
+('7', 'WORD', 'CURSO COMPLETO DE WORD', '40', '30', '2016'),
+('8', 'SAPATEADO', 'DANÇAS RITMICAS', '40', '30', '2018'),
+('9', 'COZINHA ITALIANA', 'COMO FAZER PIZZA', '40', '30', '2018'),
+('10', 'YouTuber', 'GERAR POLEMICA E GANHAR INSCRITOS', '40', '30', '2018');
+
+SELECT * FROM cursos;

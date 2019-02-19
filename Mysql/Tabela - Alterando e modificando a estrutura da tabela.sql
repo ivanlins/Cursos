fe0bea39b850c19f07fa0dd1079cf7bd8c4e97f1
Ivan <ivanlins73@gmail.com>
2019-02-19 13:11:31 -0300

    Alterando a Estrutura da Tabela (ALTER TABLE e DROP TABLE)

---
+++ Mysql/Tabela - Alterando e modificando a estrutura da tabela.sql
@@ -0,0 +1,27 @@
+use cadastro;
+desc pessoas;
+alter table pessoas
+add column profissao varchar(10);
+
+alter table pessoas
+drop column profissao;
+
+alter table pessoas
+add column profissao varchar(10) not null default'' after nome;
+
+alter table pessoas
+modify column profissao varchar(20);
+
+alter table pessoas
+modify column profissao varchar(20) not null default'';
+
+alter table pessoas
+add column codigo int first;
+
+alter table pessoas
+change column profissao prof varchar(20) not null default'';
+
+alter table pessoas
+rename to cursoCurso;
+
+select * from pessoas;

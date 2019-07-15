9cee53338d2f483d55d65a401c7ced66f8c0b01a
Ivan Lins <ivanlins73@gmail.com>
2019-07-15 18:26:30 -0300

    projeto pessoal

---
+++ Curso de JavaScript-2.0/Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60
+
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou o limite de velocidade com ${vel}Km/h, e está multado!`)
+}
+console.log(`Dirija sempre com o cinto de segurança`)
